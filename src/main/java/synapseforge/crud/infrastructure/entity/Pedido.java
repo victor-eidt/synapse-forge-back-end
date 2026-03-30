703683d09bb7dadcd1f4c4ef41464963e814838b
victor-eidt <victoreidtrl@gmail.com>
2026-03-30 12:41:54 -0300

    Adicionada entity pedido, enum status e repositório
    Criado modelo de domínio Pedido mapeado para o MongoDB na coleção pedidos, usando Lombok para gerar getters e setters. campos id, cliente, projeto, descricao, status, prazo, criadoEm e atualizadoEm. enum StatusPedido com MODELAGEM, IMPRESSAO, PINTURA, ACABAMENTO e FINALIZADO. PedidoRepository estendendo MongoRepository, com métodos findByStatus e findByClienteContainingIgnoreCase para facilitar as consultas mais comuns

---
+++ src/main/java/synapseforge/crud/infrastructure/entity/Pedido.java
@@ -0,0 +1,33 @@
+package synapseforge.crud.infrastructure.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "pedidos")
+public class Pedido {
+
+    @Id
+    private String id;
+
+    private String cliente;
+    private String projeto;
+    private String descricao;
+
+    private StatusPedido status;
+    private LocalDate prazo;
+
+    private LocalDateTime criadoEm;
+    private LocalDateTime atualizadoEm;
+}
