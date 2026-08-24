f3937790c84ce06abe391c188c1c0abc9081b442
Dirceu Cheliga <dirceuchjr@hotmail.com>
2026-08-24 16:27:44 -0300

    adicionar saldo, unidade e estoque minimo em Material

--- src/main/java/synapseforge/crud/DTO/Material/MaterialResponseDTO.java
+++ src/main/java/synapseforge/crud/DTO/Material/MaterialResponseDTO.java
@@ -2,6 +2,7 @@ package synapseforge.crud.DTO.Material;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import synapseforge.crud.infrastructure.entity.UnidadeMedida;
 
 import java.math.BigDecimal;
 
@@ -15,4 +16,7 @@ public class MaterialResponseDTO {
     private Double densidadeGcm3;
     private BigDecimal precoPorGrama;
     private Boolean ativo;
+    private UnidadeMedida unidade;
+    private BigDecimal saldo;
+    private BigDecimal estoqueMinimo;
 }
